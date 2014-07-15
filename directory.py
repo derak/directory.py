08af126ec7e9ce7870b027c6dbaa02bea1d1baba
derak <derak@opendns.com>
2014-07-15 16:51:47 -0700

    tabs to spaces

--- directory.py
+++ directory.py
@@ -23,24 +23,24 @@ class Directory(object):
         self.l = None
 
     def connect(self, url, username, password):
-		try:
-			# Create a new user in Active Directory
-			ldap.set_option(ldap.OPT_REFERRALS, 0)
+        try:
+            # Create a new user in Active Directory
+            ldap.set_option(ldap.OPT_REFERRALS, 0)
 
-			# Allows us to have a secure connection
-			ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, 0)
+            # Allows us to have a secure connection
+            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, 0)
 
-			# Open a connection
-			self.l = ldap.initialize(url)
+            # Open a connection
+            self.l = ldap.initialize(url)
 
-			# Bind/authenticate with a user with apropriate rights to add objects
-			self.l.simple_bind_s(username, password)
+            # Bind/authenticate with a user with apropriate rights to add objects
+            self.l.simple_bind_s(username, password)
                         
-		except ldap.LDAPError, e:
-			sys.stderr.write('Error connecting to LDAP server: ' + str(e) + '\n')
-			self.result['status'] = 'Error connecting to LDAP server: ' + str(e) + '\n'
-			print json.dumps(self.result)
-			sys.exit(1)
+        except ldap.LDAPError, e:
+            sys.stderr.write('Error connecting to LDAP server: ' + str(e) + '\n')
+            self.result['status'] = 'Error connecting to LDAP server: ' + str(e) + '\n'
+            print json.dumps(self.result)
+            sys.exit(1)
 
     def add_user(self, dn, attrs):
         try:
@@ -57,57 +57,60 @@ class Directory(object):
             sys.exit(1)
 
     def add_user_to_groups(self, dn, group_dn_list):
-		try:
+        try:
             # Add user to groups as member
-			mod_attrs = [( ldap.MOD_ADD, 'member', dn )]
+            mod_attrs = [( ldap.MOD_ADD, 'member', dn )]
 
-			for g in group_dn_list:
-				self.l.modify_s(g, mod_attrs)
+            for g in group_dn_list:
+                self.l.modify_s(g, mod_attrs)
 
-		except ldap.LDAPError, e:
-			sys.stderr.write('Error with LDAP addToGroups: ' + str(e) + '\n')
-			self.result['status'] = 'Error with LDAP addToGroups: ' + str(e) + '\n'
-			print json.dumps(self.result)
-			sys.exit(1)
+        except ldap.LDAPError, e:
+            sys.stderr.write('Error with LDAP addToGroups: ' + str(e) + '\n')
+            self.result['status'] = 'Error with LDAP addToGroups: ' + str(e) + '\n'
+            print json.dumps(self.result)
+            sys.exit(1)
 
 
     def set_password(self, dn, password):
-		# HERE YOU MAKE THE utf-16-le encode password
-		unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
-		password_value = unicode_pass.encode('utf-16-le')
+        # HERE YOU MAKE THE utf-16-le encode password
+        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
+        password_value = unicode_pass.encode('utf-16-le')
 
-		# change the atribute in the entry you just created
-		add_pass = [(ldap.MOD_REPLACE, 'unicodePwd', [password_value])]
+        # change the atribute in the entry you just created
+        add_pass = [(ldap.MOD_REPLACE, 'unicodePwd', [password_value])]
 
-		try:
-			self.l.modify_s(dn, add_pass)
-		except ldap.LDAPError, error_message:
-			self.result['status'] = 'Error: could not change password: ' + str(error_message) + '\n'
-			print json.dumps(self.result)
-			sys.exit(1)
-		else:
-			self.result['status'] = 'Successfully changed password \n'
+        try:
+            self.l.modify_s(dn, add_pass)
+        except ldap.LDAPError, error_message:
+            self.result['status'] = 'Error: could not change password: ' + str(error_message) + '\n'
+            print json.dumps(self.result)
+            sys.exit(1)
+        else:
+            self.result['status'] = 'Successfully changed password \n'
 
 
     def enable_user(self, dn, flag):
-		# 512 will set user account to enabled
-		# change the user to enabled
-		mod_acct = [(ldap.MOD_REPLACE, 'userAccountControl', str(flag))]
+        """Modify user, flag is userAccountControl property"""
+        # 512 will set user account to enabled
+        # change the user to enabled
+        mod_acct = [(ldap.MOD_REPLACE, 'userAccountControl', str(flag))]
 
-		try:
-			self.l.modify_s(dn, mod_acct)
-		except ldap.LDAPError, error_message:
-			self.result['status'] = 'Error to enable user: ' + str(error_message) + '\n'
-			print json.dumps(self.result)
-			sys.exit(1)
-		else:
-			self.result['status'] = 'Success enable user \n'
+        try:
+            self.l.modify_s(dn, mod_acct)
+        except ldap.LDAPError, error_message:
+            self.result['status'] = 'Error to enable user: ' + str(error_message) + '\n'
+            print json.dumps(self.result)
+            sys.exit(1)
+        else:
+            self.result['status'] = 'Success enable user \n'
 
     def disconnect(self):
-	    # disconnect
-	    self.l.unbind_s()
+        self.l.unbind_s()
+
+    def get_result(self):
+        return self.result
 
 
 if __name__ == '__main__':
-	print 'This is directory.py'
+    print 'This is directory.py'
 
